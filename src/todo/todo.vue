d78d64c86fbe5ece9d3f33b507cbacf993b606e6
v_zfahu <v_zfahu@tencent.com>
2020-12-02 22:23:17 +0800

    feat:project init

---
+++ src/todo/todo.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="real-app">
+        <input
+            autofocus="autofocus"
+            placeholder="请输入信息...."
+            class="add-input"
+            @keyup.enter="addTodo"
+            type="text">
+        <Item
+            v-for="todo in filterdTodos"
+            :key="todo.id"
+            :todo="todo"
+            @delete="deleteTodo"
+        />
+        <Tabs :filter="filter" :todos="todos" @toggle="toggleFilter" @clear="clearAllCompleted" />
+    </section>
+</template>
+
+<script>
+import Item from './item.vue';
+import Tabs from './tabs.vue';
+
+export default {
+  data() {
+    return {
+      id: 0,
+      todos: [],
+      filter: '全部',
+    };
+  },
+  components: {
+    Item,
+    Tabs,
+  },
+  methods: {
+    addTodo(e) {
+      if (e.target.value === '') {
+        alert('请输入信息再添加！');
+        return;
+      }
+      this.todos.unshift({
+        id: this.id,
+        content: e.target.value.trim(),
+        completed: false,
+      });
+      this.id += 1;
+      e.target.value = '';
+    },
+    deleteTodo(id) {
+      this.todos.splice(this.todos.findIndex((todo) => todo.id === id), 1);
+    },
+    toggleFilter(state) {
+      this.filter = state;
+    },
+    clearAllCompleted() {
+      this.todos = this.todos.filter((_) => !_.completed);
+    },
+  },
+  computed: {
+    filterdTodos() {
+      if (this.filter === '全部') {
+        return this.todos;
+      }
+      return this.todos.filter((_) => _.completed === (this.filter === '已选中'));
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+    .real-app{
+        width 600px
+        margin :0px  auto
+        box-shadow :0px 0px 5px #666
+    }
+    .add-input{
+        positon:relative;
+        margin 0px
+        width 100%
+        font-size 24px
+        font-family  inherit
+        font-weight:inherit
+        line-height 1.4rem
+        border 0;
+        outline none
+        color inherit
+        padding 6px
+        border 1px solid #999
+        box-shadow: inset 0 -1px 5px 0px rgba(0,0,0,0)
+        box-sizing border-box
+        font-smoothing:antialiased;
+        padding 16px 16px 16px 60px
+        border none
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const webpack = require('webpack');
+// 使用vue-loader必须引入此插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HTMLPlugin = require('html-webpack-plugin');
+
+const config = {
+  // 构建目标 默认值wewb
+  target: 'web',
+  // 指定入口文件
+  entry: path.join(__dirname, 'src/index.js'),
+
+  // 指定出口文件，也就是打包产物路径
+  output: {
+    filename: 'bundle.[hash:8].js',
+    path: path.join(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        // loader是rule.use.loader的简写
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // 使页面js可以获取到环境变量信息
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: isDev ? '"development"' : '"production"',
+      },
+    }),
+    new VueLoaderPlugin(),
+    new HTMLPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
+if (isDev) {
+  config.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: ['vue-style-loader', {
+      loader: 'css-loader',
+      options: {
+        // 这里有个坑，css-loader的esModule如果不设置成false,将无法生效
+        // css-loader4.0后默认对esModule设置的是true
+        // vue-style-loader默认接收的是commonjs的结果，也就是默认接收的是“css-loader中esModule设置的是false的结果”
+        esModule: false,
+      },
+    }, {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      },
+    }, 'stylus-loader'],
+  });
+  // 控制是否生成，以及如何生成 source map
+  config.devtool = 'eval-cheap-module-source-map';
+  // webpack-dev-server配置
+  config.devServer = {
+    port: 8000,
+    host: '0.0.0.0',
+    overlay: {
+      errors: true,
+    },
+    // open:true,
+    hot: true,
+    // historyFallback:{
+
+    // }
+  };
+  config.plugins.push(
+    // 局部热更新
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  );
+} else {
+  config.module.rules.push({
+    test: /\.styl(us)?$/,
+    use: [{
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        publicPath: '',
+      },
+    }, 'css-loader', {
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      },
+    }, 'stylus-loader'],
+  });
+}
+module.exports = config;
